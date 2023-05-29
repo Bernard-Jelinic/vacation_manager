970fe441afd3a5740825b5f330a73528f0eb0ccf
Bernard-Jelinic <jelinic.bernard@gmail.com>
2023-05-29 09:04:23 +0200

    Creating notification when creating vacation in seeder

--- database/seeders/VacationSeeder.php
+++ database/seeders/VacationSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Vacation;
 use App\Models\VacationStatus;
@@ -25,6 +26,33 @@ class VacationSeeder extends Seeder
                     'status_id' => $status->id,
                     'user_id' => $user->id
                 ]);
+                foreach ( $user->ancestors as $ancestor ) {
+                    if ($ancestor->role == 'admin') {
+                        $text = 'Employee ' . $user->name . ' from department ' . $user->department->name .  ' send request for vacation';
+                    } else{
+                        $text = 'Employee ' . $user->name . ' send request for vacation';
+                    }
+
+                    $vacation = Vacation::latest('id')->first();
+
+                    $randomNumber = rand(1, 100);
+                    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                    $randomString = substr(str_shuffle($characters), 0, 3); // Generates a random string of length 10
+
+                    $randomWordWithNumber = $randomString . $randomNumber;
+
+                    \DB::table('notifications')->insert(
+                        [
+                            'id' => $randomWordWithNumber,
+                            'type' => 'App\Notifications\VacationNotification',
+                            'notifiable_type' => 'App\Models\User',
+                            'notifiable_id' => $ancestor->id,
+                            'data' => '{"data":"' . $text . '","vacation_id":' . $vacation->id . '}',
+                            'created_at' => Carbon::now(),
+                            'updated_at' => Carbon::now()
+                        ]
+                    );
+                }
             }
         }
     }
